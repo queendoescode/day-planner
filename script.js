dbfedf0ed4b23e003d92bfda0b21175523510fd3
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-06-28 16:13:31 -0400

    added classes for colour coding to indicate current, past and future time.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+
 $(function () { 
   var workHours = $("#work-hours"); 
 
@@ -9,27 +10,42 @@ $(function () {
 
   for (var hour = 9; hour <= 17; hour++) {
     var amPm;
-    var hourOfDay;
-    if (hour >= 12) {
-      amPm = 'PM';
-    } else {
+    if (hour < 12) {
       amPm = 'AM';
+    } else {
+      amPm = 'PM';
     }
 
+    var hourOfDay;
     if (hour > 12) {
       hourOfDay = hour - 12;
     } else {
       hourOfDay = hour;
     }
 
+    var currentHour = dayjs().hour();
+    var isPresent = hour === currentHour;
+    var isFuture = hour > currentHour;
+
+    var colourClass;
+    if (isPresent) {
+      colourClass = "present";
+    } else if (isFuture) {
+      colourClass = "future";
+    } else {
+      colourClass = "past";
+    }
+
     var newElement = $(
-      `<div id="hour-${hour}" class="row time-block past">
+      `<div id="hour-${hour}" class="row time-block ${colourClass}">
         <div class="col-2 col-md-1 hour text-center py-3">${hourOfDay}${amPm}</div>
         <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
         <button class="btn saveBtn col-2 col-md-1" aria-label="save">
           <i class="fas fa-save" aria-hidden="true"></i>
         </button>
-      </div>`);    // create Element
+      </div>`);  
+
+   
       
     workHours.append(newElement); // add to DOM so it will be visible
   }
@@ -55,17 +71,16 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
 
   var timeElement =$("#currentDay");
+  // See: https://day.js.org/docs/en/parse/now
 
-  var timerId = setInterval(
-    () => {
-      // See: https://day.js.org/docs/en/parse/now
-      var currentTime = dayjs();
-      var formattedString = currentTime.format(" dddd,MMMM Do");
+  function everySecond() {
+    var currentTime = dayjs();
+    var formattedString = currentTime.format(" dddd,MMMM Do");
 
-      timeElement.text(formattedString);
-    },
-    1000
-  );
+    timeElement.text(formattedString);
+  }
+
+  var timerId = setInterval(everySecond, 1000);
 
 
 });
