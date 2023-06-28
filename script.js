ac7c3f06b5379aad285cbeb459393bcc06e6d595
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-06-28 17:35:38 -0400

    added an event listener to all save buttons and use DOM traversal to find time-block's ID so that we can save into the appropriate key in local storage.

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 // in the html.
 
 
-$(function () { 
+$(function () {
+  // This function is invoked after page and all its dependencies have loaded.
+
   var workHours = $("#work-hours"); 
 
   // Create the DOM elements for each working hour in the day
@@ -35,6 +37,12 @@ $(function () {
     workHours.append(newElement); // add to DOM so it will be visible
   }
 
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+
   function updateColourClasses() {
 
     var currentHour = dayjs().hour();
@@ -64,12 +72,15 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+  $(".saveBtn").on("click", function() {
+    // Inside this handler function, `this` refers to the button element that was clicked    
+    // There is nothing about the BUTTON element that indicates the hour.
+    // The event listener has to look at the parent with class time-block
+    // to find out which hour.
+
+    console.log( $(this).closest(".time-block").attr("id") );
+  });
 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
@@ -94,4 +105,6 @@ $(function () {
   var timerId = setInterval(doThisEverySecond, 1000);
 
 
+
+  
 });
